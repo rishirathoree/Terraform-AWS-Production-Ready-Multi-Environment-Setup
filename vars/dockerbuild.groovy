bef1e64c079d68c572ad3c670f646f887112a16c
rishi.rathore <rishi.rathore@liseinfotech.com>
2025-11-29 23:40:55 +0530

    added new pages

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,5 +1,5 @@
 def call(String imageName,String tag, String userName, String filePath) {
-    withCredentials([usernamePassword(credentialsId: userName, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
         sh "docker login -u $USERNAME -p $PASSWORD"
     }
     sh "docker build -t ${imageName}:${tag} -f ${filePath} ."
